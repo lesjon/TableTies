746d84eb01c1b18bb2a5e0815f7baa968f28ec3b
Leon Klute <l.klute@caesar.nl>
2023-08-03 11:42:27 +0200

    main: navigate to login when not logged in

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import KeycloakUser from "../domain/KeycloakUser";
 import {BehaviorSubject} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +10,37 @@ import {BehaviorSubject} from "rxjs";
 export class UserService {
   private userSubject = new BehaviorSubject<KeycloakUser | null>(null);
   user$ = this.userSubject.asObservable();
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
 
   getUser() {
-    const user =  this.http.get<KeycloakUser>('http://localhost:8080/api/user', { withCredentials: true });
-    user.subscribe(user => this.userSubject.next(user));
+    const user = this.http.get<KeycloakUser>('http://localhost:8080/api/user', {withCredentials: true});
+    user.subscribe(
+      {
+        next: user => this.userSubject.next(user),
+        error: () => {
+          this.userSubject.next(null)
+          this.router.navigate(['login']);
+        }
+      });
     return user;
   }
 
   logout() {
-    return this.http.post<void>('http://localhost:8080/logout', {}, { withCredentials: true });
+    return this.http.post<void>('http://localhost:8080/logout', {}, {withCredentials: true});
   }
 
   login(username: string, password: string) {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    const headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
     const body = new URLSearchParams();
     body.set('username', username);
     body.set('password', password);
-    return this.http.post<any>('http://localhost:8080/login', body.toString(), { headers: headers, withCredentials: true });
+    return this.http.post<any>('http://localhost:8080/login', body.toString(), {
+      headers: headers,
+      withCredentials: true
+    });
   }
 
   clear() {
